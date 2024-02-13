1904e154babc1f6fb8689980546c0ec517163b5e
JustBenjamin <48764617+JustBenjamin@users.noreply.github.com>
2024-02-13 14:20:17 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# paint-app
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/JustBenjamin/paint-app)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Let's Play!</h1>
+      <p>I Love React!</p>
+    </div>
+  );
+}
